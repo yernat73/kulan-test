ed075336dc2f41a2d485054931dd30318365441d
yernut <ernat.31.07.97@gmail.com>
2023-06-19 00:16:30 +0600

    feat(deliveries): create relationship manager

---
+++ app/Filament/Resources/DeliveryResource/RelationManagers/UsersRelationManager.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Resources\DeliveryResource\RelationManagers;
+
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class UsersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'users';
+
+    protected static ?string $recordTitleAttribute = 'email';
+
+    protected static ?string $inverseRelationship = 'deliveries';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\AttachAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\DetachAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DetachBulkAction::make(),
+            ]);
+    }    
+}
