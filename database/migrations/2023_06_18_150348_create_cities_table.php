b8fb1a2550575b784bccfe8326bf56d7ff5b4025
yernut <ernat.31.07.97@gmail.com>
2023-06-18 16:47:49 +0600

    feat: create cities migration & model & seeder

---
+++ database/migrations/2023_06_18_150348_create_cities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->unique();
+
+            $table->timestamps();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->foreignId('city_id')
+                ->nullable()
+                ->constrained('cities')
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('city_id');
+        });
+
+        Schema::dropIfExists('cities');
+    }
+};
---
+++ database/seeders/CitySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\City;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+class CitySeeder extends Seeder
+{
+    public function run()
+    {
+        $data = File::get(resource_path('data/kaz.json'));
+
+        collect(json_decode($data, flags: JSON_UNESCAPED_UNICODE))
+            ->values()
+            ->pluck('name_ru')
+            ->unique()
+            ->each(fn(string $name) => City::query()->create(['name' => $name]));
+    }
+}
