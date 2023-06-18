049eb232dcb556c2b98d912271af9eb4440f8854
yernut <ernat.31.07.97@gmail.com>
2023-06-18 19:04:41 +0600

    feat(filament): add users resource

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    protected static ?string $navigationGroup = 'Filament Shield';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->string()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('email')
+                    ->string()
+                    ->email()
+                    ->required()
+                    ->maxLength(255)
+                    ->unique('users', 'email', ignoreRecord: true),
+
+                Forms\Components\TextInput::make('password')
+                    ->required()
+                    ->string()
+                    ->maxLength(255)
+                    ->password()
+                    ->confirmed()
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state)),
+
+                Forms\Components\TextInput::make('password_confirmation')
+                    ->required()
+                    ->string()
+                    ->maxLength(255)
+                    ->password(),
+
+                Forms\Components\Select::make('city_id')
+                    ->relationship('city', 'name'),
+
+                Forms\Components\Select::make('roles')
+                    ->relationship('roles', 'name')
+                    ->multiple(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id'),
+                Tables\Columns\TextColumn::make('city.name'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
